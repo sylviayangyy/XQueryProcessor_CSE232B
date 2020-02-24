6f26b81f59af6dc384b90b27b3185903dbd29ffa
sljzdqd <978639043@qq.com>
2020-02-23 17:36:47 -0800

    final

--- src/main/java/XQueryProcessor.java
+++ src/main/java/XQueryProcessor.java
@@ -1,3 +1,4 @@
+
 import IO.IOHelper;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -6,30 +7,21 @@ import xquery.CustomXQueryVisitor;
 import xquery.xqueryLexer;
 import xquery.xqueryParser;
 
+import java.io.FileInputStream;
+
 public class XQueryProcessor {
     public static void main(String[] args) {
-        String xquery = "<acts> { for $a in doc(\"j_caesar.xml\")//ACT\n" +
-                "\n" +
-                "              where empty ( for $sp in $a/SCENE/SPEECH/SPEAKER\n" +
-                "\n" +
-                "                                      where $sp/text() = \"CASCA\" \n" +
-                "\n" +
-                "                                     return <speaker> {$sp/text()}</speaker> \n" +
-                "\n" +
-                "                                     )\n" +
-                "\n" +
-                "              return <act>{$a/TITLE/text()}</act>\n" +
-                "\n" +
-                "}</acts>";
-//        String xquery = "$a00";
-        ANTLRInputStream input = new ANTLRInputStream(xquery);
-        xqueryLexer lexer = new xqueryLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        xqueryParser parser = new xqueryParser(tokens);
-        ParseTree tree = parser.xq();
-//        System.out.println(tree.toStringTree());
-        CustomXQueryVisitor xQueryVisitor = new CustomXQueryVisitor();
-//        xQueryVisitor.visit(tree);
-        IOHelper.outputResult(xQueryVisitor.visit(tree));
+        try {
+            FileInputStream query = new FileInputStream(args[0]);
+            ANTLRInputStream input = new ANTLRInputStream(query);
+            xqueryLexer lexer = new xqueryLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            xqueryParser parser = new xqueryParser(tokens);
+            ParseTree tree = parser.xq();
+            CustomXQueryVisitor xQueryVisitor = new CustomXQueryVisitor();
+            IOHelper.outputResult(xQueryVisitor.visit(tree));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
