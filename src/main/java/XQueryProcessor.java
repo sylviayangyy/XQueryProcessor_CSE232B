2d831ba61bfac2f1b4348165b35c443921bf7784
yangsj <1030518209@qq.com>
2020-03-09 19:20:52 -0700

    package to jar, input as a file

--- src/main/java/XQueryProcessor.java
+++ src/main/java/XQueryProcessor.java
@@ -7,17 +7,19 @@ import xquery.CustomXQueryVisitor;
 import xquery.xqueryLexer;
 import xquery.xqueryParser;
 
+import java.io.FileInputStream;
+
 public class XQueryProcessor {
     public static void main(String[] args) {
         try {
-//            FileInputStream query = new FileInputStream(args[0]);
-            String query = "for $s in doc(\"j_caesar.xml\")//SPEAKER, $a in doc(\"j_caesar.xml\")//ACT,\n" +
-                    "\n" +
-                    "    $sp in $a//SPEAKER, $stxt in $s/text()\n" +
-                    "\n" +
-                    "where $sp eq $s and $stxt = \"CAESAR\"\n" +
-                    "\n" +
-                    "return <act> { $a/TITLE/text()} </act>";
+            FileInputStream query = new FileInputStream(args[0]);
+//            String query = "for $s in doc(\"j_caesar.xml\")//SPEAKER, $a in doc(\"j_caesar.xml\")//ACT,\n" +
+//                    "\n" +
+//                    "    $sp in $a//SPEAKER, $stxt in $s/text()\n" +
+//                    "\n" +
+//                    "where $sp eq $s and $stxt = \"CAESAR\"\n" +
+//                    "\n" +
+//                    "return <act> { $a/TITLE/text()} </act>";
             ANTLRInputStream input = new ANTLRInputStream(query);
             xqueryLexer lexer = new xqueryLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
